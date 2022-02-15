77ca41dca024e798f2be8736138fdaa01955977c
Alexandre Philibert <alexandre.philibert@cpnv.ch>
2022-02-15 14:39:34 +0100

    Add @svgr/webpack config to next.config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,14 @@ const runtimeCaching = require("next-pwa/cache");
 
 module.exports = withPWA({
   reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
   pwa: {
     dest: "public",
     runtimeCaching,
