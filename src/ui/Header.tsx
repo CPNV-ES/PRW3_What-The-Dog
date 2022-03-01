9006bedc67681d8d810945347de4b45307b885ef
Alexandre Philibert <alexandre.philibert@cpnv.ch>
2022-03-01 14:18:28 +0100

    Add SwipeableDrawer to Header

--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -1,12 +1,56 @@
+import { KeyboardEvent, MouseEvent, useState } from "react";
+import { Box } from "@mui/system";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import SwipeableDrawer from "@mui/material/SwipeableDrawer";
+
 import MenuHamburgerSVG from "../assets/menu-hamburger.svg";
 
 export const Header = () => {
+  const [open, setOpen] = useState(false);
+
+  const toggleDrawer =
+    (open: boolean) => (event: MouseEvent | KeyboardEvent) => {
+      if (
+        event &&
+        event.type === "keydown" &&
+        ((event as KeyboardEvent).key === "Tab" ||
+          (event as KeyboardEvent).key === "Shift")
+      ) {
+        return;
+      }
+
+      setOpen(open);
+    };
+
   return (
     <header className="flex p-2 bg-[#041f29]">
       <h1 className="font-bold text-white">Dashboard</h1>
-      <button className="ml-auto sm:hidden">
+      <button className="ml-auto sm:hidden" onClick={toggleDrawer(true)}>
         <MenuHamburgerSVG width={24} height={24} />
       </button>
+      <SwipeableDrawer
+        anchor="right"
+        open={open}
+        onClose={toggleDrawer(false)}
+        onOpen={toggleDrawer(true)}
+      >
+        <Box
+          sx={{ width: 250 }}
+          role="presentation"
+          onClick={toggleDrawer(false)}
+          onKeyDown={toggleDrawer(false)}
+        >
+          <List>
+            {["Services", "Clients", "Chiens"].map((text) => (
+              <ListItem button key={text}>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      </SwipeableDrawer>
     </header>
   );
 };
