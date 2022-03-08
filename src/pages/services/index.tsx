952495481104e7d42783f9bddbf96c64a9ed5fb9
SuperQuentin <quentin.aellen@cpnv.ch>
2022-03-08 11:34:31 +0100

    feat: add full_size display of services button

--- src/pages/services/index.tsx
+++ src/pages/services/index.tsx
@@ -9,6 +9,7 @@ type ServicesProps = {
 type ServiceProps = {
   name: string;
   link: string;
+  full_size?: boolean;
   image_url: string;
 };
 
@@ -17,12 +18,14 @@ const Services: NextPage<ServicesProps> = (props: ServicesProps) => {
   return (
     <div className="mx-auto max-w-3xl">
       <div className="mx-4 p-2 sm:p-4 border-2 border-[#041f29] dark:border-none dark:bg-[#17333D] rounded-lg">
-        <div className="flex flex-col sm:grid sm:grid-cols-4 gap-4">
+        <div className="flex flex-col sm:grid sm:grid-flow-row sm:grid-cols-4 gap-4">
           {services.map((service: ServiceProps, index) => {
             return (
               <Link key={index} href={service.link}>
                 <a
-                  className={`relative col-span-2 flex pt-32 justify-start items-end p-4 pb-6 rounded-xl`}
+                  className={`relative col-span-${
+                    service.full_size ? 4 : 2
+                  } flex pt-32 justify-start items-end p-4 pb-6 rounded-xl`}
                 >
                   <span className="text-white drop-shadow z-10">
                     {service.name}
@@ -53,8 +56,9 @@ export async function getStaticProps() {
         {
           name: "Coaching en comportement",
           link: "https://random.dog/",
+          full_size: true,
           image_url:
-            "https://images.unsplash.com/photo-1517849845537-4d257902454a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80",
+            "https://images.unsplash.com/photo-1530281700549-e82e7bf110d6?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=oscar-sutton-yihlaRCCvd4-unsplash.jpg&w=1920",
         },
         {
           name: "Coaching en comportement",
